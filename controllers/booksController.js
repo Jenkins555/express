660eed0a4dfba23cb1f38fd22872677deb0a20f6
Анисимов Александр <ujc90@mail.ru>
2023-10-06 06:57:53 +0300

    add

---
+++ controllers/booksController.js
@@ -0,0 +1,73 @@
+
+const express = require('express');
+const router = express.Router();
+const Book = require('../models/book');
+
+// Массив для хранения книг
+const books = [];
+
+// Получить все книги
+router.get('/', (req, res) => {
+  res.json(books);
+});
+
+// Получить книгу по ID
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const book = books.find((b) => b.id === id);
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).json({ message: 'Book not found' });
+  }
+});
+
+// Создать новую книгу
+router.post('/', (req, res) => {
+    // Извлекаем данные из тела запроса
+    const { title, description, authors, favorite, fileCover, fileName } = req.body;
+  
+    // Генерируем уникальный ID на основе времени
+    const id = Date.now().toString();
+  
+    // Создаем новый экземпляр класса Book
+    const newBook = new Book(id, title, description, authors, favorite, fileCover, fileName);
+  
+    // Добавляем новую книгу в массив books
+    books.push(newBook);
+  
+    // Отправляем успешный ответ с созданным объектом книги
+    res.status(201).json(newBook);
+  });
+
+// Редактировать книгу по ID
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, description, authors, favorite, fileCover, fileName } = req.body;
+  const book = books.find((b) => b.id === id);
+  if (!book) {
+    res.status(404).json({ message: 'Book not found' });
+  } else {
+    book.title = title;
+    book.description = description;
+    book.authors = authors;
+    book.favorite = favorite;
+    book.fileCover = fileCover;
+    book.fileName = fileName;
+    res.json(book);
+  }
+});
+
+// Удалить книгу по ID
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  const index = books.findIndex((b) => b.id === id);
+  if (index === -1) {
+    res.status(404).json({ message: 'Book not found' });
+  } else {
+    books.splice(index, 1);
+    res.json({ message: 'Book deleted' });
+  }
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+// Подключаем контроллер для работы с книгами
+const booksController = require('./controllers/booksController');
+
+// Разрешаем парсинг JSON
+app.use(express.json());
+
+// Используем маршруты для /api/books
+app.use('/api/books', booksController);
+
+// Запускаем сервер
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
